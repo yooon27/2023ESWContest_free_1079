ba51387dff1feb8de6386030f28228e49fc99ca2
Jimmy-K1m <ehdwls3588@gmail.com>
2023-07-21 20:22:33 +0900

    Added code for saving images and debug

--- test/Merge/Code_for_trainingset.py
+++ test/Merge/Code_for_trainingset.py
@@ -1,117 +1,145 @@
 import numpy as np
 import cv2
-import os
 import RPi.GPIO as GPIO
 from time import sleep
 
-# Define motor control states
+# Motor states
 STOP = 0
 FORWARD = 1
 BACKWARD = 2
 
-# Define motor channels
+# Motor channels
 CH1 = 0
 CH2 = 1
 
-# Define GPIO pin modes
+# Pin I/O settings
 OUTPUT = 1
 INPUT = 0
 
-# Define GPIO pin levels
+# Pin settings
 HIGH = 1
 LOW = 0
 
-# Define GPIO pin assignments for motor control
-ENA = 26  # GPIO pin 37
-ENB = 0   # GPIO pin 27
+# Physical pin definitions
+# PWM PIN
+ENA = 26  # 37 pin
+ENB = 0   # 27 pin
 
-IN1 = 19  # GPIO pin 37
-IN2 = 13  # GPIO pin 35
-IN3 = 6   # GPIO pin 31
-IN4 = 5   # GPIO pin 29
+# GPIO PIN
+IN1 = 19  # 37 pin
+IN2 = 13  # 35 pin
+IN3 = 6   # 31 pin
+IN4 = 5   # 29 pin
 
+
+# Pin configuration function
 def setPinConfig(EN, INA, INB):
     GPIO.setup(EN, GPIO.OUT)
     GPIO.setup(INA, GPIO.OUT)
     GPIO.setup(INB, GPIO.OUT)
 
+    # Set PWM frequency to 100kHz
     pwm = GPIO.PWM(EN, 100)
 
+    # Initially stop PWM
     pwm.start(0)
     return pwm
 
+# Motor control function
 def setMotorControl(pwm, INA, INB, speed, stat):
+    # Set motor speed with PWM
     pwm.ChangeDutyCycle(speed)
 
     if stat == FORWARD:
         GPIO.output(INA, HIGH)
         GPIO.output(INB, LOW)
 
+    # Backward
     elif stat == BACKWARD:
         GPIO.output(INA, LOW)
         GPIO.output(INB, HIGH)
 
+    # Stop
     elif stat == STOP:
         GPIO.output(INA, LOW)
         GPIO.output(INB, LOW)
 
+
+
+# Simplified motor control function
 def setMotor(ch, speed, stat):
     if ch == CH1:
+        # pwmA is the PWM handle returned after pin configuration
         setMotorControl(pwmA, IN1, IN2, speed, stat)
     else:
+        # pwmB is the PWM handle returned after pin configuration
         setMotorControl(pwmB, IN3, IN4, speed, stat)
 
-# Set GPIO mode to BCM
+
+# GPIO mode setting
 GPIO.setmode(GPIO.BCM)
 
-# Initialize PWM configurations for motor control
+# Motor pin configuration
+# Get PWM handles after pin configuration
 pwmA = setPinConfig(ENA, IN1, IN2)
-pwmB = setPinConfig(ENB, IN3, IN4)
+pwmB = setPinConfig(ENB, IN3, IN4) #ENB??
 
-# Initialize variables for car state
+# Dataset saving paths
+dataset_path = "/home/raspberry/dataset"
+
+
+dataset_path_right = "/home/raspberry/dataset/right"
+
+
+dataset_path_left = "/home/raspberry/dataset/left"
+
+
+dataset_path_forward = "/home/raspberry/dataset/forward"
+
+
+
 i = 0
 j = 0
 k = 0
 
-# Open video capture from camera
-cap = cv2.VideoCapture(0)
+cap = cv2.VideoCapture(0)  # Video file path
 
-# Initialize carstate as "stop"
 carstate = "stop"
 
 while cap.isOpened():
-    k = cv2.waitKey(10) & 0xFF  # Capture keyboard input
-    if k == 27:  # Press 'Esc' key to exit
+    key = cv2.waitKey(10) & 0xFF  # Frame rate and key detection
+    
+    if key == 27:
         break
-    elif k == 82:  # Press 'R' key to go forward
+    elif key == 82:
         print("go")
         carstate = "go"
         setMotor(CH1, 80, FORWARD)
         setMotor(CH2, 80, FORWARD)
         
-    elif k == 81:  # Press 'Q' key to go left
+    elif key == 81:
         print("left")
         carstate = "left"
         setMotor(CH1, 80, FORWARD)
         setMotor(CH2, 80, BACKWARD)
-
-    elif k == 83:  # Press 'S' key to go right
+    elif key == 83:
         print("right")
         carstate = "right"
         setMotor(CH1, 80, BACKWARD)
         setMotor(CH2, 80, FORWARD)
        
-    elif k == 84:  # Press 'T' key to stop
+    elif key == 84:
         print("stop")
         carstate = "stop"
         setMotor(CH1, 0, STOP)
         setMotor(CH2, 0, STOP)
 
-    ret, src = cap.read()  # Capture continuous frames from the camera
+    ret, src = cap.read()  # Read frames from the video
     if not ret:
         break
-
-    gray = cv2.cvtColor(src, cv2.COLOR_BGR2GRAY)  # Convert frame to grayscale
+    src = cv2.flip(src,-1)
+    
+    gray = cv2.cvtColor(src, cv2.COLOR_BGR2GRAY)  # Convert to grayscale
 
     blurred = cv2.GaussianBlur(gray, (5, 5), 0)  # Apply Gaussian blur
 
@@ -119,12 +147,28 @@ while cap.isOpened():
 
     contours, _ = cv2.findContours(thresh, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)  # Find contours
 
-    mask = np.zeros_like(src)  # Create a mask with the same size as the frame
+    mask = np.zeros_like(src)  # Create a blank mask
 
-    cv2.drawContours(mask, contours, -1, (255, 255, 255), thickness=cv2.FILLED)  # Draw filled contours on the mask
+    cv2.drawContours(mask, contours, -1, (255, 255, 255), thickness=cv2.FILLED)  # Draw contours on the mask
+
+
     
-    cv2.imshow("Result", mask)  # Display the masked frame
+    cv2.imshow("Result", mask)
 
-GPIO.cleanup()  # Clean up GPIO pins
-cap.release()  # Release video capture
-cv2.destroyAllWindows()  # Close all windows
+    if carstate == "right":
+        cv2.imwrite(f"{dataset_path_right}/right_{i:05d}.jpg", mask)
+        i += 1
+
+    elif carstate == "left":
+        cv2.imwrite(f"{dataset_path_left}/left_{j:05d}.jpg", mask)
+        j += 1
+
+    elif carstate == "go":
+        cv2.imwrite(f"{dataset_path_forward}/forward_{k:05d}.jpg", mask)
+        k += 1
+   
+
+# Cleanup GPIO
+GPIO.cleanup()
+cap.release()
+cv2.destroyAllWindows()
--- test/SW/opencv_test.py
+++ test/SW/opencv_test.py
@@ -3,38 +3,41 @@ import cv2
 import os
 
 # Dataset storage paths
-dataset_path = "C:/Users/ehdwl/Desktop/Trainingset"
-dataset_path_right = "C:/Users/ehdwl/Desktop/Trainingset/right"
-dataset_path_left = "C:/Users/ehdwl/Desktop/Trainingset/left"
-dataset_path_forward = "C:/Users/ehdwl/Desktop/Trainingset/forward"
+dataset_path = "C:/Users/dongjin/Desktop/Trainingset"
+
+dataset_path_right = "C:/Users/dongjin/Desktop/Trainingset/right"
+
+dataset_path_left = "C:/Users/dongjin/Desktop/Trainingset/left"
+
+dataset_path_forward = "C:/Users/dongjin/Desktop/Trainingset/forward"
 
 i = 0
 j = 0
 k = 0
 
 # Open the video file
-cap = cv2.VideoCapture("C:/Users/ehdwl/Desktop/road_video2.mp4")
+cap = cv2.VideoCapture("C:/Users/dongjin/Desktop/road_video.mp4")
 
 # Initialize car state to "go"
 carstate = "go"
 
 while cap.isOpened():
-    k = cv2.waitKey(10) & 0xFF  # Connect frame rate
-    if k == 27:  # Press 'Esc' key to exit
+    key = cv2.waitKey(10) & 0xFF  # Connect frame rate
+    if key == 27:  # Press 'Esc' key to exit
         break
-    elif k == 82:  # Press 'R' key to go forward
+    elif key == 82:  # Press 'R' key to go forward
         print("go forward")
         carstate = "go"
         # Motor go forward
-    elif k == 81:  # Press 'Q' key to go left
+    elif key == 81:  # Press 'Q' key to go left
         print("go left")
         carstate = "left"
         # Motor go left
-    elif k == 83:  # Press 'S' key to go right
+    elif key == 83:  # Press 'S' key to go right
         print("go right")
         carstate = "right"
         # Motor go right
-    elif k == 84:  # Press 'T' key to stop
+    elif key == 84:  # Press 'T' key to stop
         print("stop")
         carstate = "stop"
         # Motor stop
@@ -44,7 +47,6 @@ while cap.isOpened():
     if not ret:
         break
 
-    print(src.shape)
     # Crop the source frame to a specific region of interest
     cropped_src = src[100:500, :]
 
@@ -55,7 +57,7 @@ while cap.isOpened():
     blurred = cv2.GaussianBlur(gray, (5, 5), 0)
 
     # Apply binary thresholding to the blurred frame
-    _, thresh = cv2.threshold(gray, 200, 255, cv2.THRESH_BINARY)
+    _, thresh = cv2.threshold(blurred, 200, 255, cv2.THRESH_BINARY)
 
     # Find contours in the binary frame
     contours, _ = cv2.findContours(thresh, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
@@ -65,21 +67,25 @@ while cap.isOpened():
 
     # Draw filled contours on the mask
     cv2.drawContours(mask, contours, -1, (255, 255, 255), thickness=cv2.FILLED)
-
-    # Display the original and masked frames
-    cv2.imshow("Original", src)
-    cv2.imshow("Result", mask)
+    
+    
 
     # Save the frames with appropriate labels based on the car state
     if carstate == "right":
         cv2.imwrite(os.path.join(dataset_path_right, "right_%05d.jpg" % i), mask)
         i += 1
+
     elif carstate == "left":
         cv2.imwrite(os.path.join(dataset_path_left, "left_%05d.jpg" % j), mask)
         j += 1
+
     elif carstate == "go":
         cv2.imwrite(os.path.join(dataset_path_forward, "forward_%05d.jpg" % k), mask)
         k += 1
+
+    # Display the original and masked frames
+    cv2.imshow("Original", src)
+    cv2.imshow("Masked", mask)
 
 # Release the video and close all windows
 cap.release()
