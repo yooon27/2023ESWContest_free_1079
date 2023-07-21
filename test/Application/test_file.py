3043afa3a3ab611a6000ab23f4587136ceff9da4
yunseung <yunseung27@naver.com>
2023-07-21 21:16:16 +0900

    modified application test file (uploadModule function)

--- test/Application/test_file.py
+++ test/Application/test_file.py
@@ -22,4 +22,37 @@ storage = firebase.storage()
 # Upload an image file
 storage.child('image_path').put('image_path')
 
-print("complete")
+def uploadModule(latitude,longitude,image_path):
+    import pyrebase
+
+    firebaseConfig = {
+        'apiKey': "AIzxxxxxxxxxxxxxxxxxxxx",
+        'authDomain': "imxxxxxxxxxxx",
+        'databaseURL': "httpsxxxxxxxxxxxxxxx",
+        'projectId': "ixxxxxxxxxx",
+        'storageBucket': "ixxxxxxxxxxm",
+        'messagingSenderId': "1217xxxxxx",
+        'appId': "1:1217696xxx",
+        'measurementId': "GxxxxxxxxxxH"
+        }
+    #Connect Firebase
+    firebase = pyrebase.initialize_app(firebaseConfig)
+    db = firebase.database()
+    st = firebase.storage()
+    
+    #Retrieve Data
+    number = db.child("numberMarker").get()
+    
+    snum = str(number.val())
+    
+    #Upload GPS and Image File
+    st.child("image").child("image_name"+snum).put(image_path)
+    db.child("gps").child(snum+ "lati" ).set(latitude)
+    db.child("gps").child(snum+ "long").set(longitude)
+    
+    #Upload plusNumber
+    plusNumber = number.val()+1
+    db.child("numberMarker").set(plusNumber)
+
+    print("complete upload module")
+
